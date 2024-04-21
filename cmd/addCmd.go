099a0ab98cc75cb7dd9d98149b414ff289c7b648
Davit Khudaverdyan <davkhuda@gmail.com>
2024-04-21 09:30:15 -0400

    Add commands to create new projects in the Kando file

---
+++ cmd/addCmd.go
@@ -0,0 +1,14 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+)
+
+var addCmd = &cobra.Command{
+	Use:   "add",
+	Short: "Base command to add new projects or tasks to your Kando",
+}
+
+func init() {
+	addCmd.AddCommand(addProjectCmd)
+}
---
+++ cmd/addProjectCmd.go
@@ -0,0 +1,40 @@
+package cmd
+
+import (
+	"kando/kando"
+	"os"
+	"path/filepath"
+	"strconv"
+
+	"github.com/spf13/cobra"
+)
+
+var addProjectCmd = &cobra.Command{
+	Use:   "project",
+	Short: "Add a new project to your Kando file",
+	Args:  cobra.ExactArgs(1),
+	Run:   addProjectsCmdFunc,
+}
+
+func addProjectsCmdFunc(cmd *cobra.Command, args []string) {
+
+	// Make sure the argument is a string/word, not a number
+	if _, err := strconv.Atoi(args[0]); err == nil {
+		panic("Arguement must be a word entry")
+	}
+
+	// Get the Kando file from the default directory
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		panic(err)
+	}
+	kandoPath := filepath.Join(homeDir, "kando", "kando.json")
+
+	k := kando.FromFilePath(kandoPath)
+
+	k.AddProject(args[0])
+	err = k.Save()
+	if err != nil {
+		panic(err)
+	}
+}
